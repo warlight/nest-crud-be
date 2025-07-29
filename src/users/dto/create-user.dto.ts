e81f00ee780b388b228c9bb938f146a891432991
Davydov <roman.dav@intername.media>
2025-07-29 14:35:01 +0300

    validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import {IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator';
+
+export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    name: string;
+}
